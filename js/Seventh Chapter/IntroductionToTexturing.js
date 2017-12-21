fd881430b79ed002ca469a983bce9fe7872f8e82
MrSaiw <barselont@gmail.com>
2017-12-21 22:42:32 +0500

    Выполненный 1ый пример 7ой главы

--- js/Seventh Chapter/IntroductionToTexturing.js
+++ js/Seventh Chapter/IntroductionToTexturing.js
@@ -2,42 +2,30 @@ var gl;
 var shaderProgram;
 var vertexBuffer;
 var indexBuffer;
-
-var texture; // переменная для хранения текстуры
+var texture;
 
 function initShaders() {
     var fragmentShader = getShader(gl.FRAGMENT_SHADER, 'shader-fs');
     var vertexShader = getShader(gl.VERTEX_SHADER, 'shader-vs');
-
     shaderProgram = gl.createProgram();
-
     gl.attachShader(shaderProgram, vertexShader);
     gl.attachShader(shaderProgram, fragmentShader);
-
     gl.linkProgram(shaderProgram);
-
     if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-        alert("Не удалось установить шейдеры");
+        alert("Failed to install shaders");
     }
-
     gl.useProgram(shaderProgram);
-
     shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
 }
 
-// Функция создания шейдера
 function getShader(type,id) {
     var source = document.getElementById(id).innerHTML;
-
     var shader = gl.createShader(type);
-
     gl.shaderSource(shader, source);
-
     gl.compileShader(shader);
-
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        alert("Ошибка компиляции шейдера: " + gl.getShaderInfoLog(shader));
+        alert("Shader compilation error: " + gl.getShaderInfoLog(shader));
         gl.deleteShader(shader);
         return null;
     }
@@ -45,19 +33,16 @@ function getShader(type,id) {
 }
 
 function initBuffers() {
-
     var vertices =[
-                -0.5, -0.5, 0.5,
-                -0.5, 0.5, 0.5,
-                 0.5, 0.5, 0.5,
-                 0.5, -0.5, 0.5
-                 ];
-
-  vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-  vertexBuffer.itemSize = 3;
-
+        -0.5, -0.5, 0.5,
+        -0.5, 0.5, 0.5,
+        0.5, 0.5, 0.5,
+        0.5, -0.5, 0.5
+    ];
+    vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    vertexBuffer.itemSize = 3;
     var indices = [0, 1, 2, 2, 3, 0];
     indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
@@ -66,59 +51,48 @@ function initBuffers() {
 }
 
 function draw() {
-
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute,
-                         vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
-
+        vertexBuffer.itemSize, gl.FLOAT, false, 0, 0);
     gl.drawElements(gl.TRIANGLES, indexBuffer.numberOfItems, gl.UNSIGNED_SHORT,0);
-
 }
-function setupWebGL()
-{
+
+function setupWebGL() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT || gl.DEPTH_BUFFER_BIT);
-
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 }
 
 function setTextures(){
     texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
-     var image = new Image();
-
+    var image = new Image();
     image.onload = function() {
-
         handleTextureLoaded(image, texture);
-
         setupWebGL();
         draw();
-    }
-    image.src = "brickwall.png";
-
+    };
+    image.src = "../../src/images/texture/stone.jpg";
     shaderProgram.samplerUniform = gl.getUniformLocation(shaderProgram, "uSampler");
     gl.uniform1i(shaderProgram.samplerUniform, 0);
 }
- function handleTextureLoaded(image, texture) {
 
+function handleTextureLoaded(image, texture) {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
 }
-window.onload=function(){
 
+window.onload=function() {
     var canvas = document.getElementById("canvas3D");
     try {
         gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
-    }
-    catch(e) {}
-
-      if (!gl) {
-        alert("Ваш браузер не поддерживает WebGL");
-      }
-    if(gl){
+    } catch(e) {}
+    if (!gl) {
+        alert("Your browser doesn't support WebGL");
+    } if(gl){
         gl.viewportWidth = canvas.width;
         gl.viewportHeight = canvas.height;
 
@@ -127,4 +101,4 @@ window.onload=function(){
 
         setTextures();
     }
-}
+};
